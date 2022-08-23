df09598f13078b9e12671359ac1f883854342607
Carlos Garcia <94165301+cgarciaorts@users.noreply.github.com>
2022-08-23 13:09:12 +0200

    Delete libreria directory

--- libreria/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Esto es un mensaje');
-});
-
-module.exports = router;
